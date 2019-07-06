5235735b107b4eaf8c5fca5b41ce7b6d61bc3509
octobug <octobug@qq.com>
2019-07-06 18:12:57 +0800

    feature #32: download by multi-threading

--- helpers/WordHelper.py
+++ helpers/WordHelper.py
@@ -21,10 +21,13 @@ How to use:
 2. Run the script with `python3 WordHelper.py`
 3. Enjoy
 """
+from concurrent.futures import ThreadPoolExecutor
 import collections
 import json
 import os
 import urllib.request
+from urllib.error import URLError
+
 
 GOOGLE_TRANSLATE_AUDIO_API = (
     "https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q="
@@ -37,14 +40,19 @@ JSON_FILE_EXT = ".json"
 AUDIO_EXT = ".mp3"
 NEW_WORD_FILE = "./NewWords.txt"
 ASSET_DIR = "../src/audio/"
+EXEC_PATH_MSG = "This script should be run under `helpers` directory."
+VPN_MSG = "VPN is needed."
+MAX_WORKERS = 16
+
+word_db = {}
 
 
-def exec_path_prompt():
-    print("This script should be run under `helpers` directory.")
+def exec_path_prompt(msg):
+    print(msg)
     exit(0)
 
 
-def update_word_db(word_db, word, letter):
+def update_word_db(word, letter):
     """
     Update word_db by given `word`'s first letter.
     New word will be inserted into word_db only when this function return `True`
@@ -69,7 +77,7 @@ def update_word_db(word_db, word, letter):
                     print("File `{file}` created.".format(file=json_file))
             return True
     else:
-        exec_path_prompt()
+        exec_path_prompt(EXEC_PATH_MSG)
 
 
 def download_audio_resource(word):
@@ -87,12 +95,16 @@ def download_audio_resource(word):
     opener.addheaders = [("User-agent", "Mozilla/5.0")]
     urllib.request.install_opener(opener)
     print("Downloading `{0}.mp3` from Google Translate.".format(word))
-    print("url: {}".format(url))
-    urllib.request.urlretrieve(url, os.path.join(ASSET_DIR, audio_name))
+    print("URL: {}".format(url))
+    try:
+        urllib.request.urlretrieve(url, os.path.join(ASSET_DIR, audio_name))
+    except URLError as e:
+        print(f"Error: {e}")
+        exec_path_prompt(VPN_MSG)
     print("Download `{0}.mp3` successfully.".format(word))
 
 
-def insert_new_word(word_db, word, letter):
+def insert_new_word(word, letter):
     # insert new word into word_db
     new_word = {
         "spell": word,
@@ -105,7 +117,7 @@ def insert_new_word(word_db, word, letter):
     word_db[letter][word.lower()] = new_word
 
 
-def sync_word_db_to_file(word_db):
+def sync_word_db_to_file():
     for letter, words in word_db.items():
         with open(os.path.join(DATA_DIR, letter + JSON_FILE_EXT), "w") as fp:
             ordered_words = collections.OrderedDict(sorted(words.items()))
@@ -113,23 +125,29 @@ def sync_word_db_to_file(word_db):
     print("Sync word_db to file successfully.")
 
 
+def process_word(word):
+    letter = word[0].lower()
+    if update_word_db(word, letter):
+        # word does not exist in word_db
+        download_audio_resource(word)
+        insert_new_word(word, letter)
+    else:
+        print("`{0}` already exists, being skipped.".format(word))
+
+
 def main():
-    word_db = {}
     try:
+        words = []
         with open(NEW_WORD_FILE, "r") as fp:
             for line in fp.readlines():
-                word = line.strip()
-                letter = word[0].lower()
-                if update_word_db(word_db, word, letter):
-                    # word does not exist in word_db
-                    download_audio_resource(word)
-                    insert_new_word(word_db, word, letter)
-                else:
-                    print("`{0}` already exists, being skipped.".format(word))
+                words.append(line.strip())
+        workers_num = min(MAX_WORKERS, len(words))
+        with ThreadPoolExecutor(workers_num) as executor:
+            executor.map(process_word, words)
     except FileNotFoundError as e:
-        print(e)
-        exec_path_prompt()
-    sync_word_db_to_file(word_db)
+        print(f"Error: {e}")
+        exec_path_prompt(EXEC_PATH_MSG)
+    sync_word_db_to_file()
     # cleanup
     open(NEW_WORD_FILE, "w").close()
 
